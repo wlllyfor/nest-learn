2331075f2db3bd17da61495e23774bc6b2fbd028
wlllyfor <958167224@qq.com>
2023-05-25 15:26:08 +0800

    feat: add lifecycle

--- global-and-lifecycle/src/app.module.ts
+++ global-and-lifecycle/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { EventModule } from './event/event.module';
+import { CccModule } from './ccc/ccc.module';
+import { DddModule } from './ddd/ddd.module';
 
 @Module({
-  imports: [TaskModule, EventModule],
+  imports: [TaskModule, EventModule, CccModule, DddModule],
   controllers: [AppController],
   providers: [AppService],
 })

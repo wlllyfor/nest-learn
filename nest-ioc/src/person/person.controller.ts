35ee1e356b35354845e96330c5b56df57ac973b6
wlllyfor <958167224@qq.com>
2023-05-25 14:23:05 +0800

    feat: add file

--- nest-ioc/src/person/person.controller.ts
+++ nest-ioc/src/person/person.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { PersonService } from './person.service';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { CreatePersonDto } from './dto/create-person.dto';
+
 @Controller('api/person')
 export class PersonController {
   constructor(private readonly personService: PersonService) { }
@@ -21,8 +23,15 @@ export class PersonController {
   //   return `received: ${JSON.stringify(createPersonDto)}`;
   // }
 
-  @Post()
-  body(@Body() createPersonDto: CreatePersonDto) {
+  // @Post()
+  // body(@Body() createPersonDto: CreatePersonDto) {
+  //   return `received: ${JSON.stringify(createPersonDto)}`;
+  // }
+
+  @Post('file')
+  @UseInterceptors(AnyFilesInterceptor())
+  body2(@Body() createPersonDto: CreatePersonDto, @UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log(files);
     return `received: ${JSON.stringify(createPersonDto)}`;
   }
 }

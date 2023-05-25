5c8495570cfdfac6e215eed09372f721ce22549d
wlllyfor <958167224@qq.com>
2023-05-25 13:17:01 +0800

    feat: add task problem

--- nest-ioc/src/app.module.ts
+++ nest-ioc/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { ProblemModule } from './problem/problem.module';
 
 @Module({
-  imports: [],
+  imports: [TaskModule, ProblemModule],
   controllers: [AppController],
   providers: [AppService],
 })
